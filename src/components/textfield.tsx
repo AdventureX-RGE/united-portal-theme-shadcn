d519dfcd92e2aeeea2601b6c24aaccf2fd0eb589
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-08-17 23:59:34 +0800

    Refactor `TextField` component to use typed props for `placeholder`

--- src/components/textfield.tsx
+++ src/components/textfield.tsx
@@ -1,6 +1,6 @@
 import { Input } from '@/components/ui/input';
 import '../styles/global.css';
 
-export default (placeholder = '') => {
+export default ({ placeholder = '' }: { placeholder: string }) => {
   return <Input placeholder={placeholder} />;
 };
