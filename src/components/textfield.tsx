f102a69e7f0d03a0265fde8b27c0b662b7f4ffd7
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-08-19 00:30:42 +0800

    Integrate `@united-portal/apex` package to enhance `TextField` component with typed props and update project scripts/configuration accordingly.

--- src/components/textfield.tsx
+++ src/components/textfield.tsx
@@ -1,6 +1,7 @@
 import { Input } from '@/components/ui/input';
 import '../styles/global.css';
+import type { TextFieldProps } from '@united-portal/apex';
 
-export default ({ placeholder = '' }: { placeholder: string }) => {
-  return <Input placeholder={placeholder} />;
+export default ({ className, ...props }: TextFieldProps) => {
+  return <Input className={className} {...props} />;
 };
